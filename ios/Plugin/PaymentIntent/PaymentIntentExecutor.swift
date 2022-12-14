e3b9aa2e3a351af24741b293b43c4c08e909c7fd
John Moran <voneddy@gmail.com>
2022-12-14 10:00:27 +0000

    unifying resolves....

--- ios/Plugin/PaymentIntent/PaymentIntentExecutor.swift
+++ ios/Plugin/PaymentIntent/PaymentIntentExecutor.swift
@@ -6,44 +6,46 @@ class PaymentIntenetExecutor: NSObject, STPAuthenticationContext {
         return (UIApplication.shared.keyWindow?.rootViewController)!
     }
     
-    public weak var plugin: CAPPlugin?
+    // public weak var plugin: CAPPlugin?
     
     @objc func confirmPaymentIntent(_ call: CAPPluginCall) {
-            let stripeAccount = call.getString("stripeAccount") ?? ""
-            let clientSecret = call.getString("clientSecret") ?? ""
-            let paymentMethodId = call.getString("paymentMethodId") ?? ""
+        
+        let stripeAccount = call.getString("stripeAccount") ?? ""
+        let clientSecret = call.getString("clientSecret") ?? ""
+        let paymentMethodId = call.getString("paymentMethodId") ?? ""
 
-            if (clientSecret.isEmpty || paymentMethodId.isEmpty) {
-                call.reject("Missing params")
-                return
-            }
-            
-            if stripeAccount != "" {
-                STPAPIClient.shared.stripeAccount = stripeAccount
-            }
-            let pip: STPPaymentIntentParams = STPPaymentIntentParams.init(clientSecret: clientSecret)
-            pip.paymentMethodId = paymentMethodId
-            let pm = STPPaymentHandler.shared()
-            pm.confirmPayment(pip, with: self) { (status, pi, err) in
-                       switch status {
-                       case .failed:
-                           if err != nil {
-                               let errorMessage = err?.localizedDescription ?? "payment failed"
-                               self.plugin?.notifyListeners(PaymentIntentEvents.Failed.rawValue, data: ["error": errorMessage])
-                               call.reject(errorMessage)
-                           } else {
-                               self.plugin?.notifyListeners(PaymentIntentEvents.Failed.rawValue, data: ["error": "payment failed"])
-                               call.reject("payment failed")
-                           }
-
-                       case .canceled:
-                           self.plugin?.notifyListeners(PaymentIntentEvents.Cancelled.rawValue, data: ["error": "payment failed"])
-                           call.reject("user cancelled the transaction")
-
-                       case .succeeded:
-                           self.plugin?.notifyListeners(PaymentIntentEvents.Succeeded.rawValue, data: ["succeeded": pi!.allResponseFields as! PluginCallResultData])
-                           call.resolve(pi!.allResponseFields as! PluginCallResultData)
-                       }
-                   }
+        if (clientSecret.isEmpty || paymentMethodId.isEmpty) {
+        call.reject("Missing params")
+            return
         }
+
+        if stripeAccount != "" {
+            STPAPIClient.shared.stripeAccount = stripeAccount
+        }
+        
+        let pip: STPPaymentIntentParams = STPPaymentIntentParams.init(clientSecret: clientSecret)
+        pip.paymentMethodId = paymentMethodId
+        let pm = STPPaymentHandler.shared()
+        pm.confirmPayment(pip, with: self) { (status, pi, err) in
+               switch status {
+               case .failed:
+                   let errorMessage = err?.localizedDescription ?? "payment failed"
+                   if err != nil {
+                       // self.plugin?.notifyListeners(PaymentIntentEvents.Failed.rawValue, data: ["error": errorMessage])
+                       call.resolve(["error": errorMessage])
+                   } else {
+                       // self.plugin?.notifyListeners(PaymentIntentEvents.Failed.rawValue, data: ["error": "payment failed"])
+                       call.resolve(["error": errorMessage])
+                   }
+
+               case .canceled:
+                   //self.plugin?.notifyListeners(PaymentIntentEvents.Cancelled.rawValue, data: ["error": "payment failed"])
+                   call.resolve(["paymentResult": PaymentIntentEvents.Cancelled.rawValue]);
+               case .succeeded:
+                   /* self.plugin?.notifyListeners(PaymentIntentEvents.Succeeded.rawValue, data: ["succeeded": pi!.allResponseFields as! PluginCallResultData]) */
+                   call.resolve(["paymentResult": PaymentIntentEvents.Succeeded.rawValue])
+            }
+        }
+        STPAPIClient.shared.stripeAccount = nil
+    }
 }
